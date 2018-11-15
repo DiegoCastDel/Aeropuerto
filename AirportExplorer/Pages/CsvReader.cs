5f17ec705d4bdd41f40415a6a9cfa8c6138a79fd
Diego <diego.cdelgado@alumnos.udg.mx>
2018-11-15 00:48:19 -0600

    Completado (La mayoria)

---
+++ AirportExplorer/Pages/CsvReader.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+
+namespace AirportExplorer.Pages
+{
+    internal class CsvReader : IDisposable
+    {
+        private StreamReader sr;
+
+        public CsvReader(StreamReader sr, CsvHelper.Configuration.Configuration configuration)
+        {
+            this.sr = sr;
+        }
+
+        internal T GetField<T>(int v)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+
+        internal bool Read()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
